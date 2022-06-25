953aeab2a3c6693713848c9b87ca33ff660aed03
David Williston <92604983+david-tangelent@users.noreply.github.com>
2022-06-24 17:33:50 -0700

    implement flutter_riverpod state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,53 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final questionsProvider = Provider(
+  ((ref) => [
+        "What is something that made you laugh?",
+        "What are you grateful for about the current season?",
+        "What smell are you grateful for?",
+        "What is something that made you smile?",
+        "What sound are you grateful for?",
+        "What about your career or work are you grateful for?",
+        "What freedoms are you grateful for?",
+        "What one thing are you grateful for about yourself?",
+        "What in nature are you grateful for?",
+        "What song or music are you grateful for?",
+        "Who inspires you?",
+        "What opportunity are you grateful for?",
+        "What about your upbringing are you grateful for?",
+        "Who is a friend that you are grateful for?",
+        "What makes you happy to be alive?",
+        "What place are you grateful for?",
+        "What is something that always cheers you up?",
+        "What memory are you grateful for?",
+        "Did somebody help you on this day? If so, who. How did they help you?",
+        "What small thing are you grateful for?",
+        "What is something about your body or your health that you are grateful for?",
+        "What hobbies are you grateful for?",
+        "What made you feel happy today?",
+        "What are you grateful for about your family?",
+        "What is the most beautiful thing you saw today?",
+        "What is something that makes your life easier?",
+        "What is something to be grateful for in this room?",
+        "What book are you grateful for? ",
+        "Who is somebody that helped you?",
+      ]),
+);
 
 void main() {
-  runApp(MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+class MyApp extends ConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
   final String title = 'Feelgood Questions';
-  final List<String> questions = [
-    "What is something that made you laugh?",
-    "What are you grateful for about the current season?",
-    "What smell are you grateful for?",
-    "What is something that made you smile?",
-    "What sound are you grateful for?",
-    "What about your career or work are you grateful for?",
-    "What freedoms are you grateful for?",
-    "What one thing are you grateful for about yourself?",
-    "What in nature are you grateful for?",
-    "What song or music are you grateful for?",
-    "Who inspires you?",
-    "What opportunity are you grateful for?",
-    "What about your upbringing are you grateful for?",
-    "Who is a friend that you are grateful for?",
-    "What makes you happy to be alive?",
-    "What place are you grateful for?",
-    "What is something that always cheers you up?",
-    "What memory are you grateful for?",
-    "Did somebody help you on this day? If so, who. How did they help you?",
-    "What small thing are you grateful for?",
-    "What is something about your body or your health that you are grateful for?",
-    "What hobbies are you grateful for?",
-    "What made you feel happy today?",
-    "What are you grateful for about your family?",
-    "What is the most beautiful thing you saw today?",
-    "What is something that makes your life easier?",
-    "What is something to be grateful for in this room?",
-    "What book are you grateful for? ",
-    "Who is somebody that helped you?",
-  ];
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final List<String> questions = ref.watch(questionsProvider);
     return MaterialApp(
       title: title,
       theme: ThemeData(
@@ -107,7 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.navigate_next_sharp),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
